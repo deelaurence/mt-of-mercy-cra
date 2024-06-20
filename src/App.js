16de9033e8d9c78ea418630dde027064a1103ae4
deelaurence <laurie143pelumi@gmail.com>
2024-06-20 10:15:09 +0100

    new boot

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {  HelmetProvider } from 'react-helmet-async';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing';
 import AboutUs from './components/About';
@@ -6,14 +6,14 @@ import './App.css';
 
 function App() {
   return (
-    <HelmetProvider>
+   
       <Router>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/us" element={<AboutUs />} />
         </Routes>
       </Router>
-    </HelmetProvider>
+   
   );
 }
 
